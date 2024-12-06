09a67a262011aecb398f4db1a467ccec48a35b4c
vegepriyanka <pvege@charlotte.edu>
2024-12-06 12:36:43 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,38 +1,26 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
-require('dotenv').config()
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const { expressjwt: expressJWT } = require('express-jwt');
 const cors = require('cors');
-
+const bcrypt = require('bcryptjs')
 
 const bodyParser = require('body-parser');
 const path = require('path');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://final-project-p75-frontend.onrender.com/');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://final-project-p75-frontend.onrender.com/'
+  );
   res.setHeader('Access-Control-Allow-Headers', 'Content-type,Authorization');
   next();
 });
 app.use(express.json());
 app.use(cors());
-
-
-// const connection = mysql.createConnection({
-//   host: 'localhost',
-//   user: 'root',
-//   password: 'root',
-//   database: 'priya_project',
-// });
-
-const connection = mysql.createConnection({
-  host: process.env.HOST,
-  user: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DB,
-});
 
 const PORT = 3000;
 
@@ -42,23 +30,15 @@ const jwtMw = expressJWT({
   algorithms: ['HS256'],
 });
 
-let users = [
-  {
-    id: 1,
-    username: 'priyanka',
-    password: 'priyanka',
-  },
-];
-
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'sql5.freemysqlhosting.net',
+  host: process.env.HOST,
   user: process.env.DBUSER,
   password: process.env.DBPASSWD,
   database: process.env.DBNAME,
 });
 
-app.get('/api/summarydata', jwtMw,(req, res) => {
+app.get('/api/summarydata', jwtMw, (req, res) => {
   pool.getConnection((err, connection) => {
     connection.query('SELECT * FROM summarytable', function (err, rows) {
       connection.release();
@@ -81,27 +61,45 @@ app.get('/api/reportdata', jwtMw, (req, res) => {
 app.post('/api/login', (req, res) => {
   const { username, password } = req.body;
 
-  for (let user of users) {
-    if (username == user.username && password == user.password) {
-      const timeInSeconds = 1800;
-      let token = jwt.sign(
-        { id: user.id, username: user.username },
-        secretKey,
-        { expiresIn: timeInSeconds }
-      );
-      res.json({
-        success: true,
-        err: null,
-        token,
-      });
-    } else {
-      res.status(401).json({
-        success: false,
-        token: null,
-        err: 'Username or password is incorrect',
-      });
-    }
-  }
+  pool.getConnection((err, connection) => {
+    connection.query('SELECT * FROM users', function (err, rows) {
+      connection.release();
+      if (err) throw err;
+      let users = JSON.stringify(rows);
+      for (let user of JSON.parse(users)) {
+        if (username == user.username) {
+          const timeInSeconds = 1800;
+          bcrypt.compare(password, user.password, async (err, isMatch) => {
+            if (isMatch) {
+              let token = jwt.sign(
+                { id: user.id, username: user.username },
+                secretKey,
+                { expiresIn: timeInSeconds }
+              );
+              res.json({
+                success: true,
+                err: null,
+                token,
+              });
+            } else {
+              res.status(401).json({
+                success: false,
+                token: null,
+                err: 'Username or password is incorrect',
+              });
+            }
+          });
+          break;
+        } else {
+          res.status(401).json({
+            success: false,
+            token: null,
+            err: 'Username or password is incorrect',
+          });
+        }
+      }
+    });
+  });
 });
 
 app.get('/api/dashboard', jwtMw, (req, res) => {
@@ -111,19 +109,6 @@ app.get('/api/dashboard', jwtMw, (req, res) => {
   });
 });
 
-app.get('/api/settings', jwtMw, (req, res) => {
-  res.json({
-    success: true,
-    myContent: 'Settings page',
-  });
-});
-
-app.get('/api/prices', jwtMw, (req, res) => {
-  res.json({
-    success: true,
-    myContent: 'This is the price $3.99',
-  });
-});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
